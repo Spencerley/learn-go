4409087a77e32a6c0ea15e3224494925d99e2bb9
Spencer Ley <spencerley234@gmail.com>
2024-07-05 11:00:03 +0100

    make main.go run

--- variables/variables.go
+++ variables/variables.go
@@ -1,22 +1,23 @@
 package variables
 
 var greeting string = "Hello, World!"
-var number int = 33
-var isTrue bool = true
-var floatNumber float64 = 3.14
-var a, b, c, d, e int = 1, 2, 3, 4, 5
+
+// var number int = 33
+// var isTrue bool = true
+// var floatNumber float64 = 3.14
+// var a, b, c, d, e int = 1, 2, 3, 4, 5
 const PI float64 = 3.14159
 const (
-	A int = 1
+	A int     = 1
 	B float32 = 3.14
-	C string = "Hi!"
-	D bool = true
-	E = 3
-	F = 4.555
-	G = "Hello"
-  )
+	C string  = "Hi!" 
+	D bool    = true
+	E         = 3
+	F         = 4.555
+	G         = "Hello"
+)
 
-  func Change() {
+func Change() {
 	greeting = "Hello, Universe!"
 	println(greeting)
-	  }
+}
